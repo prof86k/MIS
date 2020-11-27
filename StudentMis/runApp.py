baa09430e0a8104d0c001da804f67f758209d7f7
= <=>
2020-11-27 17:24:12 +0000

    modified today

--- StudentMis/runApp.py
+++ StudentMis/runApp.py
@@ -3,7 +3,8 @@ from MainApp.main import MainInterface
 
 root = tk.Tk()
 
-main = MainInterface(master=root)
+main = MainInterface()
+main.grid(row=1,column=2,padx=5,pady=5)
 root.title(string="Student management information system")
 root.resizable(width=tk.TRUE,height=tk.TRUE)
 
